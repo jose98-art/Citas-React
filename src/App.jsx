34c3f0a11340ac804afb74ed8b8a94ed281371fc
jose santiago vazquez <guerreiro1ggg@gmail.com>
2022-10-07 10:11:52 -0500

    Terminando Proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
@@ -6,8 +6,18 @@ import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
-  const [pacientes, setPacientes] = useState([])
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? [])
+  const [paciente, setPaciente] = useState({})
 
+  
+  useEffect(() => {
+    localStorage.setItem('pacientes',JSON.stringify(pacientes))
+  },[pacientes])
+ 
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
+    setPacientes(pacientesActualizados)
+  }
 
   return (
     <div className="container mx-auto mt-5">
@@ -17,9 +27,13 @@ function App() {
           <Formulario
             pacientes = {pacientes}
             setPacientes = {setPacientes}
+            paciente = {paciente}
+            setPaciente = {setPaciente}
           />
           <ListadoPacientes
-            pacientes={pacientes}
+            pacientes = {pacientes}
+            setPaciente = {setPaciente}
+            eliminarPaciente = {eliminarPaciente}
           />
       </div>
     </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Error from "./Error";
 
-const Formulario = ({pacientes, setPacientes}) => {
+const Formulario = ({pacientes, setPacientes, paciente, setPaciente}) => {
     const [nombre, setNombre] = useState('');
     const [propietario, setPropietario] = useState('');
     const [email, setEmail] = useState('');
@@ -9,6 +9,17 @@ const Formulario = ({pacientes, setPacientes}) => {
     const [sintomas, setSintomas] = useState('');
 
     const [error, setError] = useState (false)
+
+    useEffect(()=>{
+       if(Object.keys(paciente).length > 0){//Object.keys -es un metodo de comprobar si un objeto tiene algo
+        setNombre(paciente.nombre)
+        setPropietario(paciente.propietario)
+        setEmail(paciente.email)
+        setFecha(paciente.fecha)
+        setSintomas(paciente.sintomas)
+       }
+       
+    },[paciente])
 
     const generarId = ()=>{
         const random = Math.random().toString(36).substring(2);
@@ -40,12 +51,24 @@ const Formulario = ({pacientes, setPacientes}) => {
             email, 
             fecha, 
             sintomas,
-            id:generarId()
+        }
+
+        if(paciente.id){
+            //Editando registro
+            objetoPaciente.id = paciente.id
+
+            const pacientesActualizados = pacientes.map( pacienteState  => pacienteState.id === paciente.id ? objetoPaciente : pacienteState)
+
+            setPacientes(pacientesActualizados)
+            setPaciente({})
+        }else{
+            //Nuevo registro
+            objetoPaciente.id = generarId()
+            setPacientes([...pacientes, objetoPaciente])
         }
 
         // console.log(objetoPaciente)
 
-        setPacientes([...pacientes, objetoPaciente])
 
         //Reiniciar el formulario
 
@@ -129,7 +152,7 @@ const Formulario = ({pacientes, setPacientes}) => {
             <input
              type="submit" 
              className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
-             value="Agregar paciente"
+             value={paciente.id ? 'Editar Paciente':'Agregar Paciente'}
 
              />
         </form>
